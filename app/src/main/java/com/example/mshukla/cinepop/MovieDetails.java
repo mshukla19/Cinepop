d47fc0edca4c39efc9b04a94305b7273187c50fc
Manas Shukla <mnsshukla94@gmail.com>
2015-12-20 13:26:30 +0530

    fix api key issues

---
+++ app/src/main/java/com/example/mshukla/cinepop/MovieDetails.java
@@ -0,0 +1,14 @@
+package com.example.mshukla.cinepop;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MovieDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
