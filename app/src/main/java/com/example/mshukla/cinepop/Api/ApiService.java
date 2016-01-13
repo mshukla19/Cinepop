c5ad079b7ec803b8fe2b416d8e9a5cce9b7156af
Manas Shukla <mnsshukla94@gmail.com>
2016-01-13 21:46:29 +0530

    Add Trailer and Review Model

--- app/src/main/java/com/example/mshukla/cinepop/Api/ApiService.java
+++ app/src/main/java/com/example/mshukla/cinepop/Api/ApiService.java
@@ -1,6 +1,8 @@
 package com.example.mshukla.cinepop.Api;
 
 import com.example.mshukla.cinepop.Model.MovieResults;
+import com.example.mshukla.cinepop.Model.ReviewResults;
+import com.example.mshukla.cinepop.Model.TrailerResults;
 
 import retrofit.Call;
 import retrofit.http.GET;
@@ -15,10 +17,10 @@ public interface ApiService {
         @GET("discover/movie")
         Call<MovieResults> getMovieResults(@Query("api_key") String apiKey, @Query("sort_by") String sortBy);
 
-//        @GET("/3/movie/{id}/videos")
-//        Call<TrailersResults> getTrailersResults(@Path("id") long movieId, @Query("api_key") String apiKey);
-//
-//        @GET("/3/movie/{id}/reviews")
-//        Call<ReviewResults> getReviewsResults(@Path("id") long movieId, @Query("api_key") String apiKey);
+        @GET("/3/movie/{id}/videos")
+        Call<TrailerResults> getTrailersResults(@Path("id") long movieId, @Query("api_key") String apiKey);
+
+        @GET("/3/movie/{id}/reviews")
+        Call<ReviewResults> getReviewsResults(@Path("id") long movieId, @Query("api_key") String apiKey);
 
 }
